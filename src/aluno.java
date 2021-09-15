80cd960d3939468709ec66458619a674ab434f06
filipelinodasilva <filipelinodasilva@hotmail.com>
2021-09-15 20:49:22 -0300

    Operadores ternários

--- src/aluno.java
+++ src/aluno.java
@@ -11,7 +11,7 @@ public class aluno {
 		 * else if (mediaAluno < 70) { System.out.println(" vc esta reprovado"); }
 		 * System.out.println("Nome:" + nome);
 		 
-
+		*/
 		int nota1 = 10;
 		int nota2 = 30;
 		int nota3 = 40;
@@ -19,7 +19,9 @@ public class aluno {
 		int media = 0;
 
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
-
+		
+		/*
+		//Condições lógicas com IF e ELSE
 		if (media >= 70) {
 			System.out.println("Aluno Aprovado: " + media);
 		} else if (media >= 40 && media <= 69) {
@@ -29,6 +31,12 @@ public class aluno {
 			
 		*/
 		
+		//Operadores ternários são para micro validações
+		/*
+		String saidaResultado = media >= 50 ? "Aluno Aprovado" : "Aluno Reprovado";
+		System.out.println(saidaResultado);
+		*/
+		
 		
 		
 		
@@ -36,4 +44,4 @@ public class aluno {
 		}
 
 	}
-}
+
