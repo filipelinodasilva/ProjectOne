764b1a050ab226fad470ca0a1cb8b5078a306ab6
filipelinodasilva <filipelinodasilva@hotmail.com>
2021-09-15 20:35:21 -0300

    Treinamento notas de alunos

--- src/aluno.java
+++ src/aluno.java
@@ -3,15 +3,37 @@ public class aluno {
 
 	public static void main(String[] args) {
 
-		int mediaAluno = 50;
-		String nome = "Lino";
+		/*
+		 * int mediaAluno = 50; String nome = "Lino";
+		 * 
+		 * // && se as duas condições for True, então True. if (mediaAluno >= 70 &&
+		 * nome.equals("Lino")) { System.out.println("Parabens vc está aprovado"); }
+		 * else if (mediaAluno < 70) { System.out.println(" vc esta reprovado"); }
+		 * System.out.println("Nome:" + nome);
+		 
+
+		int nota1 = 10;
+		int nota2 = 30;
+		int nota3 = 40;
+		int nota4 = 50;
+		int media = 0;
+
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		if (media >= 70) {
+			System.out.println("Aluno Aprovado: " + media);
+		} else if (media >= 40 && media <= 69) {
+			System.out.println("Aluno em recuperação: " + media);
+		} else {
+			System.out.println("Aluno reprovado: " + media);
+			
+		*/
 		
-		// && se as duas condições for True, então True.
-		if (mediaAluno >= 70 && nome.equals("Lino")) {
-			System.out.println("Parabens vc está aprovado");
-		} else if (mediaAluno < 70) {
-			System.out.println(" vc esta reprovado");
+		
+		
+		
+		
 		}
-		System.out.println("Nome:" + nome);
+
 	}
 }
