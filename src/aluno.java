0311410f8833e3d01027edc371992a286660e12d
filipelinodasilva <filipelinodasilva@hotmail.com>
2021-09-15 18:05:10 -0300

    Nome alterado

--- src/aluno.java
+++ src/aluno.java
@@ -4,10 +4,10 @@ public class aluno {
 	public static void main(String[] args) {
 
 		int mediaAluno = 50;
-		String nome = "Alex";
+		String nome = "Lino";
 		
 		// && se as duas condições for True, então True.
-		if (mediaAluno >= 70 && nome.equals("Alex")) {
+		if (mediaAluno >= 70 && nome.equals("Lino")) {
 			System.out.println("Parabens vc está aprovado");
 		} else if (mediaAluno < 70) {
 			System.out.println(" vc esta reprovado");
