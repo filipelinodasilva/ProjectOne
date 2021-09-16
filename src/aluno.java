f21a045275b2d040281ca1dbef63e1b0f53e05b2
filipelinodasilva <filipelinodasilva@hotmail.com>
2021-09-16 10:51:58 -0300

    Estrutura de repetição WHILE e DO WHILE

--- src/aluno.java
+++ src/aluno.java
@@ -10,67 +10,65 @@ public class aluno {
 		 * nome.equals("Lino")) { System.out.println("Parabens vc está aprovado"); }
 		 * else if (mediaAluno < 70) { System.out.println(" vc esta reprovado"); }
 		 * System.out.println("Nome:" + nome);
-		 
-		*/
-		int nota1 = 10;
-		int nota2 = 30;
-		int nota3 = 40;
-		int nota4 = 90;
-		int media = 0;
+		 * 
+		 */
+		/*
+		 * int nota1 = 10; int nota2 = 30; int nota3 = 40; int nota4 = 90; int media =
+		 * 0;
+		 * 
+		 * media = (nota1 + nota2 + nota3 + nota4) / 4;
+		 */
+		/*
+		 * //Condições lógicas com IF e ELSE if (media >= 70) {
+		 * System.out.println("Aluno Aprovado: " + media); } else if (media >= 40 &&
+		 * media <= 69) { System.out.println("Aluno em recuperação: " + media); } else {
+		 * System.out.println("Aluno reprovado: " + media);
+		 * 
+		 */
 
-		media = (nota1 + nota2 + nota3 + nota4) / 4;
-		
+		// Operadores ternários são para micro validações
 		/*
-		//Condições lógicas com IF e ELSE
-		if (media >= 70) {
-			System.out.println("Aluno Aprovado: " + media);
-		} else if (media >= 40 && media <= 69) {
-			System.out.println("Aluno em recuperação: " + media);
-		} else {
-			System.out.println("Aluno reprovado: " + media);
-			
-		*/
-		
-		//Operadores ternários são para micro validações
-		/*
-		String saidaResultado = media >= 50 ? "Aluno Aprovado" : "Aluno Reprovado";
-		System.out.println(saidaResultado);
-		*/
-		
+		 * String saidaResultado = media >= 50 ? "Aluno Aprovado" : "Aluno Reprovado";
+		 * System.out.println(saidaResultado);
+		 */
+
 		// Switch Case: Operações Exatas
-		
-		switch (media) {
-		case 70:
-			System.out.println("Aluno aprovado: " + media);
-			break;
+		/*
+		 * switch (media) { case 70: System.out.println("Aluno aprovado: " + media);
+		 * break;
+		 * 
+		 * default: System.out.println("Aluno tem que refazer a prova: " + media);
+		 * break; }
+		 * 
+		 * int dia = 4; switch (dia) { case 1: System.out.println("Domingo"); break;
+		 * case 2: System.out.println("Segunda"); break; case 3:
+		 * System.out.println("Terça"); break; case 4: System.out.println("Quarta");
+		 * break;
+		 * 
+		 * default: System.out.println("Qualquer dia"); break; }
+		 */
 
-		default: System.out.println("Aluno tem que refazer a prova: " + media);
-			break;
-		}
-		
-		int dia = 4;
-		switch (dia) {
-		case 1:
-			System.out.println("Domingo");
-			break;
-		case 2:
-			System.out.println("Segunda");
-			break;
-		case 3:
-			System.out.println("Terça");
-			break;
-		case 4:
-			System.out.println("Quarta");
-			break;
-			
+		/* Estrutura de repetição While */
 
-		default: System.out.println("Qualquer dia");
-			break;
+		int numero = 50;
+		/*
+		 * While valida se a condição é True, se for False ele finaliza a estrutura de
+		 * repetição
+		 */
+		// Primeiro Verifica e depois Executa.
+		while (numero <= 55) {
+			System.out.println("O numero atual é : " + numero);
+			numero++;
 		}
-		
-		
-		
-		}
+
+		int numero2 = 0;
+		do { /* Primeiro Executa e depois Verifica */
+
+			System.out.println("O numero atual é : " + numero2);
+			numero2++;
+
+		} while (numero2 <= 5);
 
 	}
 
+}
