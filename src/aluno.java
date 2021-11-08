23b6522cffe5927d220f1e2d51219e42c41743b4
filipelinodasilva <filipelinodasilva@hotmail.com>
2021-11-08 11:51:41 -0300

    8.12 - Criando uma opção de confirmação

--- src/aluno.java
+++ src/aluno.java
@@ -7,17 +7,30 @@ public class aluno {
 					
 			String carros = JOptionPane.showInputDialog("Informe a quantidade de carros?");
 			String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas");
-			System.out.println(carros);
-			System.out.println(pessoas);
+
 			
 			double carroNumero = Double.parseDouble(carros);
 			double pessoaNumero = Double.parseDouble(pessoas);
 			
-			int divisao = (int) (carroNumero / pessoaNumero);
+			int divisao = (int) (carroNumero / pessoaNumero); /*4*/
 			double resto = carroNumero % pessoaNumero;
 			
-			JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + "Carros e Sobrou. ");
-			JOptionPane.showMessageDialog(null, "Sobrou " + resto + "Carros. ");
+			int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisão? ");
+			
+			if (resposta == 0){
+				JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao);
+			}else {
+				System.out.println("Não quiz ver o resultado.");
+			}
+			
+			resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o RESTO da divisão?");
+			
+			if (resposta == 0) {
+				JOptionPane.showMessageDialog(null, "O resto da divisão é " + resto);
+			}else {
+				System.out.println("Não quiz ver o resultado");
+			}
+			
 		}
 	}
 
